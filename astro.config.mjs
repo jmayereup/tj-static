ab60f23f2d13bcd5b3bcf4cfd3dd797b18544678
Jacob Mayer <jmayereup@gmail.com>
2026-02-23 14:08:47 +0700

    updated cdn for pb pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://english.teacherjake.com',
+  site: 'https://www.teacherjake.com',
   prefetch: true,
   integrations: [
     sitemap(),
