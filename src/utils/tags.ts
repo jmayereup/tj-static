6c3e68e9b791ac4583317f56773b8078c8447caa
Jacob Mayer <jmayereup@gmail.com>
2026-02-27 11:42:57 +0700

    added support for focused-reading lesson types

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -1,7 +1,7 @@
 export const CATEGORY_FILTERS = [
   { label: 'All', id: 'all' },
   { label: 'Stories', id: 'lbl-reader' },
-  { label: 'Books', id: 'book' },
+  { label: 'Reading', id: 'book' },
   { label: 'Worksheets', id: 'worksheet' },
   { label: 'Games', id: 'grammar-game' },
   { label: 'Speaking', id: 'speaking' },
